c194329f6e6d1113524c06940d39b055e1936524
Joel Vergara <63006322+joelverg@users.noreply.github.com>
2024-02-22 00:04:08 +0800

    initial commit

---
+++ src/app/components/todo/todo.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  newTodo: string = '';
+  todos: string[] = [];
+
+  addTodo() {
+    if (this.newTodo.trim() !== '') {
+      this.todos.push(this.newTodo);
+      this.newTodo = '';
+    }
+  }
+
+  deleteTodo(todo: string) {
+    this.todos = this.todos.filter(item => item !== todo);
+  }
+
+}
